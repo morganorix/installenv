bbda493eb550ff1721aee01915774e9ba1cb5d0b
Morgan JOURDIN <morgan@pouco.ooo>
2024-04-23 10:30:51 +0200

    Update samba.sh
    amélioration

--- include/samba.sh
+++ include/samba.sh
@@ -6,18 +6,18 @@ samba() {
     installpkg "samba"
 
     cat 2>/dev/null > ${PWD}/.smbcreds <<"EOF"
-username=xxxxx
-password=xxxxxxxxxxxx
+username=<username>
+password=<pwd>
 EOF
     [ ! "$?" -ne 0 ] && text "Info" "Le fichier .smbcreds est créé et configuré" || text "Error" "Impossible de configurer le fichier .smbcreds"
     
     text "Info" "Mise en place des permissions pour le fichier .smbcreds"
-    pretty chown morganorix:morganorix ${PWD}/.smbcreds 2>/dev/null
+    pretty chown <user>:<group> ${PWD}/.smbcreds 2>/dev/null
     [ ! "$?" -ne 0 ] && text "Info" "Persmissions effectué avec success." || text "Error" "Impossible de mettre les permissions en place."
     
     pretty mkdir ${PWD}/BACKUP 
     sudo sh -c "cat 2>/dev/null << EOF >> /etc/fstab
-//xxx.xxx.x.xx/BACKUP ${PWD}/BACKUP cifs _netdev,vers=3.0,users,noauto,credentials=${PWD}/.smbcreds 0 0
+//<IP ADDRESS>/BACKUP ${PWD}/BACKUP cifs _netdev,vers=3.0,users,noauto,credentials=${PWD}/.smbcreds 0 0
     "
     [ ! "$?" -ne 0 ] && text "Info" "Le fichier /etc/fstab a été modifié" || text "Error" "Impossible de modifier le fichier /etc/fstab"
     pretty mount ${PWD}/BACKUP
